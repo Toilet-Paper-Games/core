5f7e6f8555d662e68778de118e60f7f80d39f9c3
Justin Fernald <jfernald@my.gcu.edu>
2023-05-08 15:29:52 -0700

    Update package.json

--- core/index.ts
+++ core/index.ts
@@ -1,3 +1,4 @@
 export * from '@/common/BaseCommunicationHandler';
+export * from '@/common/CommunicationDataTransfers';
 export * from '@/controller/CommunicationHandlerFrameController';
 export * from '@/hoster/CommunicationHandlerFrameHoster';
