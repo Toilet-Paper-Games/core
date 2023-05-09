35de63a230cb706c19183fbe17c2b0c826cae2db
Justin Fernald <jfernald@my.gcu.edu>
2023-05-09 22:41:11 +0000

    fix failing case

--- test/src/src.test.ts
+++ test/src/src.test.ts
@@ -3,14 +3,13 @@ import { expect, test } from 'vitest';
 import { clamp, DEG_TO_RAD } from '@/index'; // This is the package itself
 
 // Edit an assertion and save to see HMR in action
-test('PACKAGE', () => {
-  test('clamp()', () => {
-    expect(clamp(0, 0, 0)).toBe(0);
-  });
+test('clamp()', () => {
+  expect(clamp(0, 0, 0)).toBe(0);
+});
 
-  test('deg_to_rad', () => {
-    expect(DEG_TO_RAD).toBe(0.017453292519943295);
-    // purposefully fail
-    expect(DEG_TO_RAD).toBe(0);
-  });
+test('deg_to_rad', () => {
+  console.log("HIIII")
+  expect(DEG_TO_RAD).toBe(0.017453292519943295);
+  // purposefully fail
+  expect(DEG_TO_RAD).toBe(0);
 });
